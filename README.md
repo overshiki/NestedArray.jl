b2b66244ae8ba9627a0e0c0f0fc9436a86af8e02
overshiki <le.niu@hotmail.com>
2022-10-23 20:36:55 +0800

    rename to NestedVector

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# NestedArray.jl
+# NestedVector.jl
 Vector{Vector{...}} style is convenient. Representing Multi-dimentional Array in a nested vector form.
---
+++ src/NestedVector.jl
@@ -0,0 +1,4 @@
+module NestedVector
+include("nsvector.jl")
+export len, find_index, find_unique_index, (++), fullstack, concat, Empty, nvsize, squeeze, cast, transpose, hsplit, nvbroadcast
+end # module
